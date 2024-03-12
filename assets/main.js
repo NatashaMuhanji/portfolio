c877240ed6fab657e0774959021db74f7613ae15
NatashaMuhanji <natashamuhanji@gmail.com>
2024-03-12 16:39:35 +0300

    Files

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+// Navigation 
+const bar = document.querySelector("#bar");
+const nav = document.querySelector("#navbar");
+const close = document.querySelector("#close");
+
+//show navigation bar
+if (bar) {
+    bar.addEventListener("click", function () {
+        nav.classList.add("active");
+    });
+}
+
+//close navigation bar
+if (close) {
+    close.addEventListener("click", function () {
+        nav.classList.remove("active");
+    });
+}
+
+//footer year
+year=document.querySelector("#contact > div.year > p > span#year").innerHTML = new Date().getFullYear();
